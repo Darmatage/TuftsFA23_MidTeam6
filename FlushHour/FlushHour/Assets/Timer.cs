98dc6b45752aa9a76632e94be98c8ced65b661b0
Joe-Ansel Puplava <Joe-Ansel.Puplava@tufts.edu>
2023-10-03 18:39:07 -0400

    Timer Crash Patched
    Fixed the issue where the game would end but the timer would crash the game.

--- FlushHour/FlushHour/Assets/Timer.cs
+++ FlushHour/FlushHour/Assets/Timer.cs
@@ -7,40 +7,59 @@ using System.Threading;
 
 public class Timer : MonoBehaviour {
 
-      //Object variables
-      public GameObject timeText;
-      public GameObject gameOverText;
-      public int gameTime = 20;
-      private float gameTimer = 0f;
+    //Object variables
+    public GameObject timeText;
+    public GameObject gameOverText;
+    public int gameTime = 20;
+    private float gameTimer = 0f;
+    private float displayTime = 3.0f;
 
-      void Start(){
-           gameOverText.SetActive(false);
-           UpdateTime();
-      }
+    void Start(){
+        gameOverText.SetActive(false);
+        UpdateTime();
+    }
 
-      void FixedUpdate(){
-            gameTimer += 0.01f;
-            if (gameTimer >= 1f){
-                        gameTime -= 1;
-                        gameTimer = 0;
-                        UpdateTime();
-            }
-            if (gameTime <= 0){
-                        gameTime = 0;
-                        gameOverText.SetActive(true);
-                        Thread.Sleep(6000);
-                        SceneManager.LoadScene("MainMenu");
-            }
-      }
+    void FixedUpdate()
+    {
+        gameTimer += 0.01f;
+        if (gameTimer >= 1f)
+        {
+            gameTime -= 1;
+            gameTimer = 0;
+            UpdateTime();
+        }
+        if (gameTime <= 0)
+        {
+            gameTime = 0;
+            StartCoroutine(DisplayGameOver());
 
-      void spawnNPCs(){
+
+        }
+    }
+    private IEnumerator DisplayGameOver()
+    {
+        // Display the game object
+        gameOverText.SetActive(true);
+        // Wait for the specified display time
+        yield return new WaitForSeconds(displayTime);
+
+        // Hide the game object
+        gameOverText.SetActive(false);
+        SceneManager.LoadScene("MainMenu");
+
+        // You can perform any additional cleanup or actions here
+
+        // Coroutine is finished
+    }
+
+    void spawnNPCs(){
             if (gameTime > 0){
                 //   Instantiate(NPCPrefab, spawnPoint.position, Quaternion.identity);
             }
-      }
+    }
 
-      public void UpdateTime(){
-            Text timeTextB = timeText.GetComponent<Text>();
-            timeTextB.text = "" + gameTime;
-      }
+    public void UpdateTime(){
+        Text timeTextB = timeText.GetComponent<Text>();
+        timeTextB.text = "" + gameTime;
+    }
 }
